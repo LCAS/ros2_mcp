fb435567a81d7158387f62b3fe2457a540e71f35
Marc Hanheide <marc@hanheide.net>
2025-07-18 14:02:25 +0000

    style: standardize string quotes and formatting in setup and test files

--- src/ros2_mcp/setup.py
+++ src/ros2_mcp/setup.py
@@ -1,33 +1,30 @@
 from setuptools import find_packages, setup
 
-package_name = 'ros2_mcp'
+package_name = "ros2_mcp"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
     install_requires=[
-        'setuptools',
-        'mcp[cli]',
-        'rclpy',
-        'cv_bridge',
-        'opencv-python',
-        'rosidl_runtime_py'
+        "setuptools",
+        "mcp[cli]",
+        "rclpy",
+        "cv_bridge",
+        "opencv-python",
+        "rosidl_runtime_py",
     ],
     zip_safe=True,
-    maintainer='Marc Hanheide',
-    maintainer_email='marc@hanheide.net',
-    description='An MCP for ROS2',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="Marc Hanheide",
+    maintainer_email="marc@hanheide.net",
+    description="An MCP for ROS2",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'ros2_mcp_server = ros2_mcp.ros2_mcp_server:main'
-        ],
+        "console_scripts": ["ros2_mcp_server = ros2_mcp.ros2_mcp_server:main"],
     },
 )
